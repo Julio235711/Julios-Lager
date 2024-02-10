2d80388efe0a5089a841e8de951f31af5c30a561
Julio235711 <159634320+Julio235711@users.noreply.github.com>
2024-02-10 17:18:15 +0100

    Add files via upload

---
+++ Calcular el Area de un Círculo .py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+import math
+print ("Ingrese el radio del Círculo.")
+r = float(input()) # Capturando el Radio.
+phi = math.pi * r
+print ("Valor de PI:",phi)
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
